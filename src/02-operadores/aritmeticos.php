f6f539a9d72ccb47def230f5b70ed298cd296738
Uanderson Rocha <urochatech@gmail.com>
2026-05-04 15:10:29 -0300

    feat: adiciona exercícios de operadores aritméticos

---
+++ src/02-operadores/aritmeticos.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Operadores Aritméticos
+ * São usados para realizar operações matemáticas entre valores numéricos.
+ * Os principais operadores aritméticos em PHP são:
+ * 1. Adição (+): Soma dois valores.
+ * 2. Subtração (-): Subtrai um valor de outro.
+ * 3. Multiplicação (*): Multiplica dois valores.
+ * 4. Divisão (/): Divide um valor por outro.
+ * 5. Módulo (%): Retorna o resto da divisão entre dois valores.
+ * 6. Exponenciação (**): Eleva um valor à potência de outro.
+ */
+
+// Operador de adição
+$valor1 = 100;
+$valor2 = 550;
+$resultado = $valor1 + $valor2;
+
+echo " A soma do valor {$valor1} com o valor {$valor2} é igual a {$resultado}." . "<br>" . "\n";
