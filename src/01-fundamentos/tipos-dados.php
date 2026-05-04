e5b67bf6f7e3cbfa3c98404ef7379505d18a42bb
Uanderson Rocha <urochatech@gmail.com>
2026-05-04 14:13:12 -0300

    feat: adiciona exercicos de tipo de dados (int, float, string, bool e null)

---
+++ src/01-fundamentos/tipos-dados.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * O php suporta 8 tipos de dados primitivos:
+ * Divididos em 3 grupos:
+ * 1. Escalares: string, int, float, bool
+ * 2. Compostos: array, object
+ * 3. Especiais: resource, null
+ */
+ // INTEIRO
+ $idadeAluno = 20; // tipo inteiro (int)
+ echo "A idade do aluno é $idadeAluno." . "<br>" . "\n";   
+?>  
