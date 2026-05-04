cb41cce6d4b600f64a991966947b83befbb89033
Uanderson Rocha <urochatech@gmail.com>
2026-05-04 11:24:36 -0300

    feat: transforma index em gerador de menu dinâmico

--- src/index.php
+++ src/index.php
@@ -1,4 +1,37 @@
 <?php
+/**
+ * Gerador de Menu Dinâmico
+ * Lista todos os diretórios e arquivos .php dentro de src
+ */
 
-use FFI\CData;
-require_once __DIR__ . '/constantes.php';
+$path = __DIR__;
+$items = scandir($path);
+
+echo "<h1>Meus Exercícios de PHP</h1>";
+echo "<ul>";
+
+foreach ($items as $item) {
+    // Ignora o próprio index e pastas ocultas do sistema/git
+    if ($item !== '.' && $item !== '..' && $item !== 'index.php') {
+        
+        $fullPath = $path . DIRECTORY_SEPARATOR . $item;
+        
+        if (is_dir($fullPath)) {
+            echo "<li><strong>[Pasta] $item</strong>";
+            
+            // Lista arquivos dentro da subpasta
+            $subItems = scandir($fullPath);
+            echo "<ul>";
+            foreach ($subItems as $sub) {
+                if (pathinfo($sub, PATHINFO_EXTENSION) === 'php') {
+                    echo "<li><a href='$item/$sub'>$sub</a></li>";
+                }
+            }
+            echo "</ul></li>";
+        } elseif (pathinfo($item, PATHINFO_EXTENSION) === 'php') {
+            echo "<li><a href='$item'>$item</a></li>";
+        }
+    }
+}
+
+echo "</ul>";
