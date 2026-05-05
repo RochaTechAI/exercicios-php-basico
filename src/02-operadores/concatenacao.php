904da148c498bfa44b9de8f9737e4fce2821c1f2
Uanderson Rocha <urochatech@gmail.com>
2026-05-05 13:57:07 -0300

    feat: adiciona exercicio de concatenacao

---
+++ src/02-operadores/concatenacao.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * O operadore de contacenação é utilizado para unir duas ou mais strings em uma única string.
+ * O operador de concatenação em PHP é o ponto (.) e é usado para concatenar strings.
+ * Exemplo:
+ * $string1 = "Olá, ";
+ * $string2 = "mundo!";
+ * $resultado = $string1 . $string2; // O resultado será "Olá, mundo!"
+ * O operador de concatenação pode ser usado para criar mensagens dinâmicas,
+ * combinar variáveis e strings literais, ou construir strings complexas a partir 
+ * de partes menores.  
+ */
+$string1 = "Olá, ";
+$string2 = "mundo!";
+$resultado = $string1 . $string2;
+echo $resultado . "<br>" . "\n"; // Saída: "Olá, mundo!"    
