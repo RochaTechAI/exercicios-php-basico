ab6d6e040ece54727dbc91a17e2a7fe8632becab
Uanderson Rocha <urochatech@gmail.com>
2026-05-05 13:51:00 -0300

    feat: adiciona operadores aritmeticos que faltavam

--- src/02-operadores/aritmeticos.php
+++ src/02-operadores/aritmeticos.php
@@ -17,3 +17,44 @@ $valor2 = 550;
 $resultado = $valor1 + $valor2;
 
 echo " A soma do valor {$valor1} com o valor {$valor2} é igual a {$resultado}." . "<br>" . "\n";
+
+// Operador de subtração
+
+$valor1 = 100;
+$valor2 = 550;
+$resultado = $valor1 - $valor2;
+echo "A subtração do valor {$valor1} com o valor {$valor2} é igual a {$resultado}." . "<br>" . "\n";
+
+// Operador de multiplicação
+$valor1 = 100;
+$valor2 = 550;
+$resultado = $valor1 * $valor2;
+echo "A multiplicação do valor {$valor1} com o valor {$valor2} é igual a {$resultado}." . "<br>" . "\n";
+
+// Operador de divisão
+$valor1 = 100;
+$valor2 = 550;  
+$resultado = $valor1 / $valor2;
+echo "A divisão do valor {$valor1} com o valor {$valor2} é igual a {$resultado}." . "<br>" . "\n";
+
+// Operador de módulo
+$valor1 = 100;
+$valor2 = 550;     
+$resultado = $valor1 % $valor2;
+echo "O módulo do valor {$valor1} com o valor {$valor2} é igual a {$resultado}." . "<br>" . "\n";   
+
+// Aritimeticos unários
+/**
+ * Os operadores unários utilizam um operador para 
+ * incrementar ou decrementar o valor de uma variável.
+ * 1. Incremento (++) - Aumenta o valor da variável em 1.
+ * 2. Decremento (--) - Diminui o valor da variável em 1.
+ */
+
+$operador = 10;
+$operador++; // Incremento
+echo "O valor do operador após o incremento é: {$operador}." . "<br>" . "\n";   
+$operador--; // Decremento
+echo "O valor do operador após o decremento é: {$operador}." . "<br>" . "\n";   
+
+
