f2c5c2538d4497ad81ba914730941293605bad95
Uanderson Rocha <urochatech@gmail.com>
2026-05-07 10:17:58 -0300

    docs: acrescenta operadores que estava faltando

--- src/02-operadores/logicos.php
+++ src/02-operadores/logicos.php
@@ -21,6 +21,42 @@ if($valor1 and $valor2){ // Avalia se valor1 e valor2 são verdadeiros
     echo 'Falso'; // se um ou ambos forem falsos    
 }
 echo "<br>" . "\n";
-?>
 
+//OPERADOR OR (||)
+
+$valor1 = true;
+$valor2 = false;
+
+if($valor1 or $valor2){ // Avalia se valor1 ou valor2 são verdadeiros
+    echo 'Verdadeiro'; // se um ou ambos forem verdadeiros
+}else{
+    echo 'Falso'; // se ambos forem falsos    
+}
+echo "<br>" . "\n";
+
+//OPERADOR XOR (XOR)
+$valor1 = true;
+$valor2 = true;
+
+If($valor1 xor $valor2){
+    echo 'verdadeiro';
+}else{
+    echo 'falso'; // se ambos forem verdadeiros ou ambos forem falsos, retorna falso    
+}
+echo "<br>" . "\n";
+
+//OPERADOR NOT (!)
+$valor1 = true; 
+if($valor1){ // Avalia se valor1 é verdadeiro
+    echo 'Verdadeiro'; // se valor1 for verdadeiro
+}else{  
+    echo 'Falso'; // se valor1 for falso    
+}
+echo "<br>" . "\n";
+
+// TENARÁRIO
+$idade = 18;
+$resultado = ($idade >= 18) ? 'Maior de idade' : 'Menor de idade'; // Avalia se a idade é maior ou igual a 18
+echo $resultado; // Imprime 'Maior de idade' se a condição for verdadeira, caso contrário, imprime 'Menor de idade'
+echo "<br>" . "\n"; 
 
